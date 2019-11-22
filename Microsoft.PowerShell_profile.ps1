eb9fb3ae4225ac224e5fe8a21e40e783b9575661
David Houston <houstdav000@gmail.com>
2019-11-22 11:00:04 -0500

    Add PowerShell Profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+New-Alias edit vim
+New-Alias wc3270 "C:\Program Files\wc3270\wc3270.exe"
+New-Alias mono 'C:\Program Files\Mono\bin\mono.exe'
+New-Alias MpCmdRun 'C:\Program Files\Windows Defender\MpCmdRun.exe'
+New-Alias firefox 'C:\Program Files\Firefox Nightly\firefox.exe'
+New-Alias git-bash 'C:\Program Files\git\git-bash.exe'
