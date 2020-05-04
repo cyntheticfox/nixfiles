abcae3ce14748f6440ae5622a6b8d2c2aefa08b9
David Houston <houstdav000@gmail.com>
2020-05-04 12:28:07 -0700

    Add conditional execution for fish path and starship init to config.fish

--- config/config.fish
+++ config/config.fish
@@ -1,7 +1,19 @@
+#!/usr/local/bin/fish
+# Set PROFILE variable for powershell-like referencing of this file
+set -U profile (status fish-path)
+
 # Create Alias for ls
-alias ls="ls -Al"
+alias ls="ls -A -l"
+
+# Edit path
+for each in ~/.cargo/bin
+    if test -d $each
+        and not contains $each $PATH
+        set -a PATH $each
+    end
+end
 
 # Set up starship
-set PATH ~/.cargo/bin $PATH
-
-starship init fish | source
+if test -x ~/.cargo/bin/starship
+    starship init fish | source
+end
