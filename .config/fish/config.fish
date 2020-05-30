f7000714df64855d2043670242933573fe5c8d20
David Houston <houstdav000@gmail.com>
2020-05-30 16:45:28 -0400

    Change fisher installation to be bootstrapped

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,12 @@ set -U profile ~/.config/fish/config.fish
 # Create Alias for dir
 alias dir="la"
 
+# Install fisher if available
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+end
+
 # Edit path
 for each in ~/.cargo/bin ~/.nix-profile/bin
     if test -d $each
