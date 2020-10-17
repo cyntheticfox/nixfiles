8aa52aabd0449c26e11ffa97325f3462caa99a3c
David Houston <houstdav000@gmail.com>
2020-10-17 14:06:03 -0400

    Add install.sh installation script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# Installation script
+
+for line in $(find . -name '.git*' -prune \
+    -o -name 'install.sh' -prune \
+    -o -name 'README.md' -prune \
+    -o -name 'LICENSE' -prune \
+    -o -name 'settings.json' -prune \
+    -o -type f -print); do
+    if [ -e "$HOME/$line"]; then
+        echo "$HOME/$line exists, not replacing"
+    else
+        cp $line "$HOME/$line"
+    fi
+done
