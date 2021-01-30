3f0ca630e184f430cc0ea3d91d14aa9654c74b11
David Houston <houstdav000@gmail.com>
2021-01-30 08:11:18 -0500

    Remove legacy exclude for Microsoft Windows Terminal config

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ for line in $(find . -name '.git*' -prune \
     -o -name 'install.sh' -prune \
     -o -name 'README.md' -prune \
     -o -name 'LICENSE' -prune \
-    -o -name 'settings.json' -prune \
     -o -type f -print); do
     if [ -e "$HOME/$line"]; then
         echo "$HOME/$line exists, not replacing"
