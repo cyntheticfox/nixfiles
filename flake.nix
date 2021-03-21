37247c33cc3ea2f903c6c3539daa1eb8e92a5c51
David Houston <houstdav000@gmail.com>
2021-03-21 10:34:41 -0400

    Realize you can combine sets and add a devShell

--- flake.nix
+++ flake.nix
@@ -4,31 +4,36 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }@inputs: {
-    nixosModules.dotfiles = ({ config, ... }: {
-      home.file = {
-        ".profile".source = ./.profile;
-        ".bashrc".source = ./.bashrc;
-        ".bash_profile".source = ./.bash_profile;
-        ".editorconfig".source = ./.editorconfig;
-        ".zshrc".source = ./.zshrc;
+  outputs = { self, nixpkgs, flake-utils }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        devShell =
+          pkgs.mkShell { nativeBuildInputs = with pkgs; [ shfmt nixfmt ]; };
+      }) // {
+        nixosModules.dotfiles = ({ config, ... }: {
+          home.file = {
+            ".profile".source = ./.profile;
+            ".bashrc".source = ./.bashrc;
+            ".bash_profile".source = ./.bash_profile;
+            ".editorconfig".source = ./.editorconfig;
+            ".zshrc".source = ./.zshrc;
 
-        ".config" = {
-          source = ./.config;
-          recursive = true;
-        };
+            ".config" = {
+              source = ./.config;
+              recursive = true;
+            };
 
-        ".ssh" = {
-          source = ./.ssh;
-          recursive = true;
-        };
+            ".ssh" = {
+              source = ./.ssh;
+              recursive = true;
+            };
 
-        ".gnupg" = {
-          source = ./.gnupg;
-          recursive = true;
-        };
-
+            ".gnupg" = {
+              source = ./.gnupg;
+              recursive = true;
+            };
+          };
+        });
       };
-    });
-  };
 }
