f6f9ef94742be33085b5224c6a61649bff938364
David Houston <houstdav000@gmail.com>
2021-03-21 08:08:53 -0400

    Change from package to nixosModule

--- flake.nix
+++ flake.nix
@@ -4,59 +4,31 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }@inputs:
+  outputs = { self, nixpkgs, flake-utils }@inputs: {
+    nixosModules.dotfiles = ({ config, ... }: {
+      home.file = {
+        ".profile".source = ./.profile;
+        ".bashrc".source = ./.bashrc;
+        ".bash_profile".source = ./.bash_profile;
+        ".editorconfig".source = ./.editorconfig;
+        ".zshrc".source = ./.zshrc;
 
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in rec {
-        packages.dotfiles = with pkgs;
-          stdenv.mkDerivation {
-            name = "dotfiles";
-            src = ./.;
-            installPhase = ''
-              mkdir $out
-              find $src \
-                -mindepth 1 \
-                -maxdepth 1 \
-                -not -name $src \
-                -not -name ".git" \
-                -not -name "flake.nix" \
-                -not -name "flake.lock" \
-                -not -name "install.sh" \
-                -not -name ".gitattributes" \
-                -not -name ".gitignore" \
-                -not -name "LICENSE" \
-                -not -name "README.md" \
-                -exec cp -r {} $out \;
-            '';
-          };
+        ".config" = {
+          source = ./.config;
+          recursive = true;
+        };
 
-          defaultPackage = packages.dotfiles;
+        ".ssh" = {
+          source = ./.ssh;
+          recursive = true;
+        };
 
-          nixosModules.dotfiles = ({ config, ... }: {
-            home.file = {
-              ".profile".source = ./.profile;
-              ".bashrc".source = ./.bashrc;
-              ".bash_profile".source = ./.bash_profile;
-              ".editorconfig".source = ./.editorconfig;
-              ".zshrc".source = ./.zshrc;
+        ".gnupg" = {
+          source = ./.gnupg;
+          recursive = true;
+        };
 
-              ".config" = {
-                source = ./.config;
-                recursive = true;
-              };
-
-              ".ssh" = {
-                source = ./.ssh;
-                recursive = true;
-              };
-
-              ".gnupg" = {
-                source = ./.gnupg;
-                recursive = true;
-              };
-
-            };
-          });
-        });
+      };
+    });
+  };
 }
