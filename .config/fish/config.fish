f48bfdd626ea326f335de2946f9eadc17570df80
David Houston <houstdav000@gmail.com>
2021-06-23 06:54:35 -0400

    Add fish syntax and indentation checker

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,11 +12,11 @@ set -U fish_greeting ""
 
 # Create Alias for vim
 switch $EDITOR
-    case "nvim"
+    case nvim
         alias edit="nvim"
         alias vim="nvim"
         alias vi="nvim"
-    case "vim"
+    case vim
         alias edit="vim"
         alias nvim="vim"
         alias vi="vim"
@@ -47,8 +47,8 @@ end
 
 # Edit path
 for each in ~/.cargo/bin \
-        ~/.nix-profile/bin \
-        ~/.local/bin
+    ~/.nix-profile/bin \
+    ~/.local/bin
     if test -d $each
         and not contains $each $PATH
         set -a PATH $each
@@ -57,7 +57,7 @@ end
 
 
 # Set up exa
-if command -s exa &> /dev/null
+if command -s exa &>/dev/null
     alias ls='exa -F --icons'
 else
     alias ls='ls -F'
@@ -67,27 +67,27 @@ alias la='ls -abghl'
 alias dir='ls -abghl'
 
 # Set up bat
-if command -s bat &> /dev/null
+if command -s bat &>/dev/null
     alias cat='bat'
 end
 
 # Alias ripgrep for grep
-if command -s rg &> /dev/null
+if command -s rg &>/dev/null
     alias grep='rg'
     alias egrep='rg'
     alias pgrep='rg'
 end
 
 # Alias fd for find
-if command -s fd &> /dev/null
+if command -s fd &>/dev/null
     alias find='fd'
 end
 
 # Set up starship
-if command -s starship &> /dev/null
+if command -s starship &>/dev/null
     starship init fish | source
 end
 
-if command -s direnv &> /dev/null
+if command -s direnv &>/dev/null
     eval (direnv hook fish)
 end
