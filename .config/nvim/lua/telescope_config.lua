988049fd5f08db39fb33e709d2ecafdf0c875746
David Houston <houstdav000@gmail.com>
2021-07-14 17:03:44 -0400

    Remove deprecated option

--- .config/nvim/lua/telescope_config.lua
+++ .config/nvim/lua/telescope_config.lua
@@ -27,7 +27,6 @@ require('telescope').setup{
         file_sorter = require'telescope.sorters'.get_fuzzy_file,
         file_ignore_patterns = {},
         eneric_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
-        shorten_path = true,
         winblend = 0,
         border = {},
         borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
