5788c43f9bdcf629ae1c0cd776ba4aa05b42d0ae
David Houston <houstdav000@gmail.com>
2021-12-14 15:28:12 -0500

    Move fontconfig to home-manager config
    Signed-off-by: David Houston <houstdav000@gmail.com>

--- home-manager/config/gui/fonts.nix
+++ home-manager/config/gui/fonts.nix
@@ -1,13 +1,43 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, lib, ... }: {
   home.packages = with pkgs; [
-    cascadia-code
     dejavu_fonts
     fira
     fira-code-symbols
     fira-mono
-    hack-font
-    hasklig
     nerdfonts
-    vistafonts
   ];
+
+  xdg.configFile."fontconfig/fonts.conf".text = lib.mkDefault ''
+    <?xml version="1.0"?>
+    <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+    <fontconfig>
+        <match target="pattern">
+            <test qual="any" name="family"><string>emoji</string></test>
+            <edit name="family" mode="assign" binding="same"><string>Noto Color Emoji</string></edit>
+        </match>
+
+        <match target="pattern">
+            <test qual="any" name="family"><string>emoji</string></test>
+            <edit name="family" mode="assign" binding="same"><string>Fira Code Nerd Font</string></edit>
+        </match>
+
+        <alias>
+            <family>monospace</family>
+            <prefer>
+                <family>Fira Code Nerd Font Mono</family>
+                <family>Noto Color Emoji</family>
+            </prefer>
+        </alias>
+
+        <selectfont>
+            <rejectfont>
+                <pattern>
+                    <patelt name="family">
+                        <string>DejaVu Sans Mono</string>
+                    </patelt>
+                </pattern>
+            </rejectfont>
+        </selectfont>
+    </fontconfig>
+  '';
 }
