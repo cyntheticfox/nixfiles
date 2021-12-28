e853f9594bcb805e8ca89817e97aee23ec8dfe7b
David Houston <houstdav000@gmail.com>
2021-12-28 11:25:03 -0500

    Add playerctl support via mpris2 to mpd

--- home-manager/config/tui/music.nix
+++ home-manager/config/tui/music.nix
@@ -1,10 +1,15 @@
 { config, pkgs, ... }: {
-  home.packages = with pkgs; [ ncmpcpp ];
+  home.packages = with pkgs; [ vimpc ];
 
   services.mpd = {
     enable = true;
     musicDirectory = config.xdg.userDirs.music;
     playlistDirectory = "${config.services.mpd.musicDirectory}/_playlists";
+  };
+
+  services.mpdris2 = {
+    enable = true;
+    notifications = true;
   };
 
   programs.beets = {
