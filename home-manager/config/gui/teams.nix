1a4cb680fe34906d37cb9125567c33ad25b803fd
David Houston <houstdav000@gmail.com>
2021-12-29 22:38:36 -0500

    Add a fix for Teams constantly unlinking mimeapps

--- home-manager/config/gui/teams.nix
+++ home-manager/config/gui/teams.nix
@@ -1,7 +1,16 @@
-{ pkgs, config, ... }: {
+{ pkgs, config, lib, ... }: {
   home.packages = with pkgs; [
     teams
   ];
 
   xdg.mimeApps.defaultApplications."x-scheme-handler/msteams" = "teams.desktop";
+
+  # For whatever reason, teams likes to overwrite the mimetypes, even if it's
+  #   fine. So, add a step to activation to remove the file if it's not a link.
+  #
+  home.activation.remove-mimeapps = lib.hm.dag.entryBetween [ "writeBoundary" ] [ "checkLinkTargets" ] ''
+    if [ ! -L ${config.xdg.configHome}/mimeapps.list ]; then
+        $DRY_RUN_CMD rm $VERBOSE_ARG ${config.xdg.configHome}/mimeapps.list
+    fi
+  '';
 }
