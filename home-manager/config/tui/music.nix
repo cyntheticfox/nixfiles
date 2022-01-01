acb5b15577a77f393bf3d892e30a42f756042368
David Houston <houstdav000@gmail.com>
2022-01-01 01:02:09 -0500

    Add vimpc config file

--- home-manager/config/tui/music.nix
+++ home-manager/config/tui/music.nix
@@ -1,6 +1,28 @@
 { config, pkgs, ... }: {
   home.packages = with pkgs; [ vimpc ];
 
+  ### Add config file for vimpc
+  # Strangely, the help documentation and manpage both state that the only file
+  #   checked is at "~/.vimpcrc". I tried to get that to work, but it never
+  #   loaded. Using `strace`, I found the only location it _ACTUALLY_ checks is
+  #   "$XDG_CONFIG_HOME/vimpc/vimpcrc". Weird.
+  #
+  xdg.configFile."vimpc/vimpcrc".text = ''
+    " Set the default window to show at startup
+    set window library
+    set windows library,lists,playlist
+
+    set albumartist
+    set autoscroll
+    set groupignorethe
+    set incsearch
+    set mouse
+    set reconnect
+    set seekbar
+    set singlequit
+    set smartcase
+  '';
+
   services.mpd = {
     enable = true;
     musicDirectory = config.xdg.userDirs.music;
