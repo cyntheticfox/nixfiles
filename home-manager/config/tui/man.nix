1c1783afdf63a07cc12bd1b5717872fae5a7f248
David Houston <houstdav000@gmail.com>
2022-01-13 06:27:09 -0500

    Add some manpages

--- home-manager/config/tui/man.nix
+++ home-manager/config/tui/man.nix
@@ -1,4 +1,14 @@
 { config, pkgs, ... }: {
+  home.packages = with pkgs; [
+    libcap_manpages
+    linux-manual
+    llvmPackages_latest.clang-manpages
+    llvmPackages_latest.llvm-manpages
+    manpages
+    posix_man_pages
+    stdmanpages
+  ];
+
   programs.man = {
     enable = true;
     generateCaches = true;
