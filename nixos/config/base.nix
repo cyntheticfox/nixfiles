4886f9e2ee97a92f8f241ff1ed7aa94a66ed0270
David Houston <houstdav000@gmail.com>
2022-02-21 19:08:24 -0500

    Change Nix settings to new layout

--- nixos/config/base.nix
+++ nixos/config/base.nix
@@ -1,7 +1,5 @@
 { config, lib, pkgs, self, inputs, outputs, ... }: {
   home-manager = {
-    useUserPackages = true;
-    useGlobalPkgs = true;
     sharedModules = [
       ({ pkgs, ... }: {
         programs.nix-index.enable = true;
@@ -29,19 +27,12 @@
         };
       })
     ];
+
+    useGlobalPkgs = true;
+    useUserPackages = true;
   };
 
   nix = {
-    allowedUsers = [ "@wheel" ];
-    binaryCachePublicKeys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-    binaryCaches = [
-      "https://cache.nixos.org"
-      "https://nix-community.cachix.org"
-    ];
-    buildCores = 0;
     checkConfig = true;
     extraOptions = ''
       experimental-features = nix-command flakes
@@ -52,18 +43,27 @@
       dates = "weekly";
       persistent = true;
     };
-    maxJobs = "auto";
     # nixPath = (lib.mapAttrsToList (n: v: n + "=" + v) inputs) ++ [ ("ospkgs=" + "../.") ("nixpkgs-overlays=" + ../. + "/overlays.nix") ];
     optimise.automatic = true;
-    requireSignedBinaryCaches = true;
-    useSandbox = true;
     registry = lib.mapAttrs (_: flake: { inherit flake; }) (lib.filterAttrs (_: v: v ? outputs) inputs);
+    settings = {
+      allowed-users = [ "@wheel" ];
+      cores = 0;
+      max-jobs = "auto";
+      require-sigs = true;
+      sandbox = true;
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
   };
 
-  nixpkgs = {
-    config.allowUnfree = true;
-    # overlays = lib.attrValues outputs.overlays;
-  };
+  nixpkgs.config.allowUnfree = true;
 
   environment.etc."nix/nixpkgs-config.nix".text = lib.mkDefault ''
     {
@@ -74,16 +74,16 @@
   environment = {
     defaultPackages = with pkgs; [
       bc
-      file
-      tree
-      unzip
       cachix
+      file
       git
       gnupg
-      neofetch
       nix-index
+      neofetch
       progress
       strace
+      tree
+      unzip
     ];
 
     homeBinInPath = true;
