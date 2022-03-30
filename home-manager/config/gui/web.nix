5c6d3d95fd472a304803fb6b6a42e43b945aa919
David Houston <houstdav000@gmail.com>
2022-03-30 06:35:17 -0400

    home-manager: Change Qutebrowser Search Engine
    Change the search engine from the default of Duckduckgo to Startpage

--- home-manager/config/gui/web.nix
+++ home-manager/config/gui/web.nix
@@ -2,6 +2,7 @@
 
 let
   font = "FiraMono Nerd Font";
+  homepage_url = "https://www.startpage.com/do/mypage.pl?prfe=ed8e0b7c6a6177ac1e11ccf45f0fbf02eda7eaea24fa3ca22ff3c5be22f5db78fb207ddd54bd6a552a601512394049130d39a57321213f59c677c2ddab0f2b3779ab65fe2e7d1a56920e52462f";
 in
 {
   home.sessionVariables.BROWSER = "${config.programs.qutebrowser.package}/bin/qutebrowser";
@@ -53,6 +54,10 @@ in
         last_close = "close";
         title.format = "{audio}{current_title}";
       };
+      url = {
+        default_page = homepage_url;
+        start_pages = homepage_url;
+      };
       window.title_format = "Qutebrowser - {perc}{private}{current_title}";
       zoom.default = "90%";
     };
@@ -60,6 +65,7 @@ in
       "J" = "tab-prev";
       "K" = "tab-next";
     };
+    searchEngines."DEFAULT" = "https://www.startpage.com/do/search?query={}";
   };
 
   xdg.mimeApps.defaultApplications =
