6a9376aa1261e34ba953f54fadf673e0db3a741c
David Houston <houstdav000@gmail.com>
2022-05-06 15:15:08 -0400

    Remove most unused debugging tools

--- home-manager/config/tui/dbg.nix
+++ home-manager/config/tui/dbg.nix
@@ -1,46 +1,17 @@
 { config, pkgs, ... }: {
   home.packages = with pkgs; [
     # General binary tools
-    bbe
     bintools
     bingrep
     bloaty
-    bsdiff
-    extrude
-    pev
-    poke
-
-    # ELF tools
-    chelf
-    elf-dissector
-    elf-header-real
-    #elfcat
-    elfinfo
-    elfkickers
-    elfutils
-    elfx86exts
-    patchelf
-    statifier
 
     # C/General
-    debugedit
-    edb
     gdb
     lldb
     valgrind
     rr
 
-    # Bash
-    bashdb
-
-    # Go
-    delve
-
-    # USB Devices
-    hid-listen
-
     # RE/General
-    apktool
     cutter
     ghidra
     radare2
