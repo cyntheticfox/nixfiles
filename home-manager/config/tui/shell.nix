f242fffa31400064f9309c59ea411387371bd479
David Houston <houstdav000@gmail.com>
2022-06-07 07:28:45 -0400

    Add more nix zsh aliases

--- home-manager/config/tui/shell.nix
+++ home-manager/config/tui/shell.nix
@@ -40,16 +40,59 @@
 
       # Nix flakes
       "n" = "nix";
+
       "nb" = "nix build";
+      "nbr" = "nix build --rebuild";
+
+      "nd" = "nix develop";
+
       "nf" = "nix flake";
       "nfc" = "nix flake check";
+      "nfcl" = "nix flake clone";
       "nfi" = "nix flake init";
       "nfl" = "nix flake lock";
+      "nfm" = "nix flake metadata";
+      "nfs" = "nix flake show";
       "nfu" = "nix flake update";
-      "nfsw" = "nixos-rebuild switch --flake . --use-remote-sudo";
+
+      "nfmt" = "nix fmt";
+
+      "nlog" = "nix log";
+
+      "nos" = "nixos-rebuild";
+      "nosb" = "nixos-rebuild build";
+      "nosbo" = "nixos-reubuild boot";
+      "nose" = "nixos-rebuild edit";
+      "nossw" = "nixos-rebuild switch --use-remote-sudo";
+      "nosswf" = "nixos-rebuild switch --use-remote-sudo --flake .";
+      "nosswrb" = "nixos-rebuild switch --use-remote-sudo --rollback";
+      "nost" = "nixos-rebuild test";
+      "nosv" = "nixos-rebuild build-vm";
+      "nosvb" = "nixos-rebuild build-vm-with-bootloader";
+
+      "np" = "nix profile";
+      "nph" = "nix profile history";
+      "npi" = "nix profile install";
+      "npl" = "nix profile list";
+      "npu" = "nix profile upgrade";
+      "nprm" = "nix profile remove";
+      "nprb" = "nix profile rollback";
+      "npw" = "nix profile wipe-history";
+
       "nr" = "nix run";
+
+      "nre" = "nix repl";
+
+      "nreg" = "nix registry";
+
       "ns" = "nix search";
       "nsn" = "nix search nixpkgs";
+      "nsm" = "nix search nixpkgs-master";
+
+      "nsh" = "nix shell";
+      "nshn" = "nix shell nixpkgs";
+
+      "nst" = "nix store";
     };
 
     enableAutosuggestions = true;
