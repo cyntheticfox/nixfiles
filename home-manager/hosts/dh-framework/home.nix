61b04d2b97a181f1ba720e8005515a48366593c4
David Houston <houstdav000@gmail.com>
2022-07-20 10:36:59 -0400

    Add a few gui programs thanks to WSLg support

--- home-manager/hosts/dh-framework/home.nix
+++ home-manager/hosts/dh-framework/home.nix
@@ -5,11 +5,16 @@
   imports = [
     ../../config/base.nix
 
+    # GUI modules
+    ../../config/gui/fonts.nix
+    ../../config/gui/remmina.nix
+
     # Terminal modules
     ../../config/tui/cloud.nix
     ../../config/tui/dbg.nix
     ../../config/tui/dev.nix
     ../../config/tui/documents.nix
+    ../../config/tui/music.nix
     ../../config/tui/rss.nix
   ];
 
