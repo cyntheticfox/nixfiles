57e1c0d0bcf591ac43f51b5829f4d6701ad07f48
David Houston <houstdav000@gmail.com>
2022-07-22 08:17:05 -0400

    Add some more cloud tools

--- home-manager/config/tui/cloud.nix
+++ home-manager/config/tui/cloud.nix
@@ -3,12 +3,39 @@
     # CLIs
     awscli2
     azure-cli
+    backblaze-b2
     linode-cli
+    pulumi-bin
+    rancher
 
     # Provisioning Tools
     ansible
+    nixpkgs-unstable.nodePackages.cdktf-cli
+    helm
+    istioctl
     kubectl
+    ocm
+    odo
     packer
     terraform
+
+    # Generators
+    terracognita
+
+    # Locking tools
+    terragrunt
+
+    # Checking tools
+    chart-testing
+    datree
+    kube-hunter
+    kube-linter
+    terrascan
+    tflint
+    tfsec
+
+    # Cost tools
+    cloud-custodian
+    infracost
   ];
 }
