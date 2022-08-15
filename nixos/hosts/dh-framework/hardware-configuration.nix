68e663dbb7c525ed223f6e509927fdfba6c93b6e
David Houston <houstdav000@gmail.com>
2022-08-14 20:14:42 -0400

    dh-framework: Add tlp bat thresholds

--- nixos/hosts/dh-framework/hardware-configuration.nix
+++ nixos/hosts/dh-framework/hardware-configuration.nix
@@ -133,6 +133,9 @@
     settings = {
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
+
+      START_CHARGE_THRESH_BAT0 = 75;
+      STOP_CHARGE_THRESH_BAT0 = 80;
     };
   };
 }
