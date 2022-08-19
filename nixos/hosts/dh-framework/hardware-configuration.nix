5f7482abcb72188d66f57db11e030cebd85b3265
David Houston <houstdav000@gmail.com>
2022-08-19 09:54:07 -0400

    Fixup modprobe some more

--- nixos/hosts/dh-framework/hardware-configuration.nix
+++ nixos/hosts/dh-framework/hardware-configuration.nix
@@ -48,10 +48,9 @@
     extraModprobeConfig = ''
       options v4l2loopback video_nr=63
       options kvm_intel nested=1
-      options i915 enable_guc=2 enable_gvt=true
     '';
     kernelModules = [ "v4l2loopback" ];
-    extraModulePackages = with config.boot.kernelPackages; [ acpi_call ];
+    extraModulePackages = with config.boot.kernelPackages; [ acpi_call v4l2loopback ];
   };
 
   fileSystems =
