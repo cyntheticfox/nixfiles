8c54bcdf63b11ed19b816c8de1c3be93412d0bce
David Houston <houstdav000@gmail.com>
2022-08-24 09:26:12 -0400

    lock down some kernel settings

--- nixos/config/hardware-base.nix
+++ nixos/config/hardware-base.nix
@@ -37,8 +37,47 @@ let
 in
 {
   boot = {
-    # Filter ARP packets to be responded on per-interface. Not sure why this isn't the default
-    kernel.sysctl."net.ipv4.conf.all.arp_filter" = 1;
+    kernel.sysctl = {
+      # Prevent unintentional fifo writes
+      "fs.protected_fifos" = 2;
+
+      # Prevent unintended writes to already-created files
+      "fs.protected_regular" = 2;
+
+      # Disable SUID binary dump
+      "fs.suid_dumpable" = 0;
+
+      # Require user to have CAP_SYSLOG to use dmesg
+      "kernel.dmesg_restrict" = 1;
+
+      # Prevent printing kernel pointers
+      "kernel.kptr_restrict" = 2;
+
+      # Disable late module loading
+      "kernel.modules_disabled" = 1;
+
+      # Disallow profiling at all levels without CAP_SYS_ADMIN
+      "kernel.perf_event_paranoid" = 3;
+
+      # Disable "Sysrq" key
+      "kernel.sysrq" = 0;
+
+      # Require CAP_BPF to use bpf
+      "kernel.unprvileged_bpf_disabled" = 1;
+
+      # Require process to have CAP_SYS_PTRACE and use PTRACE_ATTACH or PTRACE_TRACEME
+      "kernel.yama.ptrace_scope" = 2;
+
+      # Filter ARP packets to be responded on per-interface. Not sure why this isn't the default
+      "net.ipv4.conf.all.arp_filter" = 1;
+
+      # Filter Reverse Path
+      "net.ipv4.conf.all.rp_filter" = 1;
+
+      # Log impossible addr packets
+      "net.ipv4.conf.all.log_martians" = 1;
+      "net.ipv4.conf.default.log_martians" = 1;
+    };
 
     loader = {
       efi.canTouchEfiVariables = true;
