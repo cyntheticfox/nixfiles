01fa72ea2948a841b7d066731936a37d0d143c3b
David Houston <houstdav000@gmail.com>
2022-09-06 07:01:33 -0400

    Add printer config

--- nixos/config/services/cupsd.nix
+++ nixos/config/services/cupsd.nix
@@ -1,7 +1,11 @@
 { config, ... }: {
-  services.printing = {
-    enable = true;
-    browsing = false;
-    startWhenNeeded = true;
-  };
+  services.printing.enable = true;
+
+  hardware.printers.ensureDefaultPrinter = "Home-Printer";
+  hardware.printers.ensurePrinters = [{
+    description = "Brother HL-3170CDW";
+    deviceUri = "ipp://192.168.5.11:631/ipp";
+    model = "everywhere";
+    name = "Home-Printer";
+  }];
 }
