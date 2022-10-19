69620f903a928e8bcc18b0f2e5354621d159394d
David Houston <houstdav000@gmail.com>
2022-10-19 06:59:22 -0400

    dh-framework: Remove top comments, unused arg
    Signed-off-by: David Houston <houstdav000@gmail.com>

--- nixos/hosts/dh-framework/configuration.nix
+++ nixos/hosts/dh-framework/configuration.nix
@@ -1,10 +1,4 @@
-# configuration.nix
-#
-# Edit this configuration file to define what should be installed on
-#  your system.  Help is available in the configuration.nix(5) man page
-#  and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, lib, pkgs, ... }: {
+{ config, pkgs, ... }: {
   # Import other configuration files
   imports = [
     ./hardware-configuration.nix
