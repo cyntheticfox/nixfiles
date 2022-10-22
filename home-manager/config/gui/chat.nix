e196b96c8fbd8465eb4aca6654569015902d660a
David Houston <houstdav000@gmail.com>
2022-10-22 08:54:41 -0400

    hm: Add Discord settings w/ update override
    Signed-off-by: David Houston <houstdav000@gmail.com>

--- home-manager/config/gui/chat.nix
+++ home-manager/config/gui/chat.nix
@@ -5,4 +5,10 @@
   ];
 
   home.sessionVariables."NIXOS_OZONE_WL" = 1;
+
+  xdg.configFile."discord/settings.json".source = (pkgs.formats.json { }).generate "settings.json" {
+    IS_MAXIMIZED = true;
+    IS_MINIMIZED = false;
+    SKIP_HOST_UPDATE = true;
+  };
 }
