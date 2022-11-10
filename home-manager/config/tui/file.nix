ac6ce8ad7afd600ac80d8f82884a2893bb66d48b
David Houston <houstdav000@gmail.com>
2022-11-10 14:55:41 -0500

    hm: Add alias for glow
    Signed-off-by: David Houston <houstdav000@gmail.com>

--- home-manager/config/tui/file.nix
+++ home-manager/config/tui/file.nix
@@ -1,7 +1,12 @@
 { pkgs, ... }: {
+  home.shellAliases = {
+    "glow" = "glow -p";
+  };
+
   home.packages = with pkgs; [
     file
     fd
+    glow
     nixos-unstable.fq
     p7zip
     ripgrep
