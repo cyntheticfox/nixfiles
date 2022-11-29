fbcc3e56b5368bd4b8410d4d3d2c0269548afe5e
David Houston <houstdav000@gmail.com>
2022-11-29 08:30:54 -0500

    hc: Add GnuPG publickeys
    Signed-off-by: David Houston <houstdav000@gmail.com>

--- home-manager/config/tui/gnupg.nix
+++ home-manager/config/tui/gnupg.nix
@@ -4,33 +4,23 @@
   programs.gpg = {
     enable = true;
 
+    publicKeys = [
+      { source = ../../../keys/users/david.pub.asc; }
+    ] ++ (builtins.map (file: { source = ../../../keys/trusted + "/${file}"; }) (builtins.attrNames (builtins.readDir ../../../keys/trusted)));
+
     settings = {
-      weak-digest = "SHA1";
-      personal-cipher-preferences = [
-        "AES256"
-        "AES192"
-        "AES"
-      ];
-      personal-digest-preferences = [
-        "SHA512"
-        "SHA384"
-        "SHA256"
-      ];
-      personal-compress-preferences = [
-        "bzip2"
-        "zlib"
-        "zip"
-      ];
+      # weak-digest = "SHA1";
       default-new-key-algo = "ed25519/cert,sign+cv25519/encr";
       default-cert-expire = "3y";
       default-sig-expire = "3y";
       trust-model = "tofu+pgp";
-      keyid-format = "long";
+      keyid-format = "0xlong";
     };
   };
 
   services.gpg-agent = {
     enable = true;
+
     defaultCacheTtl = 3600;
     defaultCacheTtlSsh = 3600;
     pinentryFlavor = "curses";
