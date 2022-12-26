3957a87320b4901e6555955d3623bf084b757855
David Houston <houstdav000@gmail.com>
2022-12-26 08:25:14 -0500

    hm: Change music config to use Mopidy over mpd
    Signed-off-by: David Houston <houstdav000@gmail.com>

--- homeModules/sys/music.nix
+++ homeModules/sys/music.nix
@@ -11,97 +11,149 @@ in
 
     desktopPackages = mkOption {
       type = with types; listOf package;
+
       default = with pkgs; [
         audacity
         elisa
         lmms
         soundkonverter
       ];
+
       description = ''
         What desktop music packages to install. Typically includes both editors and players.
       '';
     };
   };
 
-  config = mkIf cfg.enable {
-    home.packages = with pkgs; [ vimpc ] ++ cfg.desktopPackages;
+  config = mkIf cfg.enable (
+    let
+      musicDir = config.xdg.userDirs.music or "$XDG_MUSIC_DIR";
+    in
+    {
+      home.packages = with pkgs; [
+        mpc-cli
+        vimpc
+      ] ++ cfg.desktopPackages;
 
-    ### Add config file for vimpc
-    # Strangely, the help documentation and manpage both state that the only file
-    #   checked is at "~/.vimpcrc". I tried to get that to work, but it never
-    #   loaded. Using `strace`, I found the only location it _ACTUALLY_ checks is
-    #   "$XDG_CONFIG_HOME/vimpc/vimpcrc". Weird.
-    #
-    # TODO: Convert to own module
-    #
-    xdg.configFile."vimpc/vimpcrc".text = ''
-      " Set the default window to show at startup
-      set window library
-      set windows library,lists,playlist
+      ### Add config file for vimpc
+      # Strangely, the help documentation and manpage both state that the only file
+      #   checked is at "~/.vimpcrc". I tried to get that to work, but it never
+      #   loaded. Using `strace`, I found the only location it _ACTUALLY_ checks is
+      #   "$XDG_CONFIG_HOME/vimpc/vimpcrc". Weird.
+      #
+      # TODO: Convert to own module
+      #
+      xdg.configFile."vimpc/vimpcrc".text = ''
+        " Set the default window to show at startup
+        set window library
+        set windows library,lists,playlist
 
-      set albumartist
-      set autoscroll
-      set groupignorethe
-      set incsearch
-      set mouse
-      set reconnect
-      set seekbar
-      set singlequit
-      set smartcase
+        set albumartist
+        set autoscroll
+        set groupignorethe
+        set incsearch
+        set mouse
+        set reconnect
+        set seekbar
+        set singlequit
+        set smartcase
 
-      map J :tabprevious<C-M>
-      map K :tabnext<C-M>
-    '';
-
-    services.mpd = {
-      enable = true;
-      musicDirectory = config.xdg.userDirs.music or "$HOME/music";
-      playlistDirectory = "${config.services.mpd.musicDirectory}/_playlists";
-      extraConfig = ''
-        audio_output {
-          type "pipewire"
-          name "PipeWire Sound Server"
-        }
+        map J :tabprevious<C-M>
+        map K :tabnext<C-M>
       '';
-    };
 
-    services.mpdris2 = {
-      enable = true;
-      notifications = true;
-    };
+      # # TODO: Fix or watch https://github.com/MusicPlayerDaemon/MPD/issues/773
+      # services.mpd = {
+      #   enable = true;
+      #
+      #   musicDirectory = musicDir;
+      #   playlistDirectory = "${config.services.mpd.musicDirectory}/_playlists";
+      #   extraConfig = ''
+      #     audio_output {
+      #       type "pipewire"
+      #       name "PipeWire Sound Server"
+      #     }
+      #   '';
+      # };
 
-    programs.beets = {
-      enable = true;
+      services.mopidy = {
+        enable = true;
 
-      settings = {
-        match = {
-          required = "title artist";
-          ignored_media = [
-            "Data CD"
-            "DVD"
-            "DVD-Video"
-            "Blu-ray"
-            "HD-DVD"
-            "VCD"
-            "SVCD"
-            "UMD"
-            "VHS"
-          ];
-        };
-        directory = config.xdg.userDirs.music or "$HOME/music";
-        library = "${config.xdg.dataHome or "$XDG_DATA_HOME"}/beets/musiclibrary.db";
-        import = {
-          languages = "en jp";
-          log = "${config.xdg.dataHome or "$XDG_DATA_HOME"}/beets/import.log";
-          move = true;
-        };
-        format_item = "$albumartist - $album - $track $title";
-        paths = {
-          default = "$albumartist - $album%aunique{}/$track - $title";
-          singleton = "etc/$artist - $title";
-          comp = "Various Artists - $album%aunique{}/$track - $title ($artist)";
+        extensionPackages = with pkgs; [
+          mopidy-local
+          mopidy-mpd
+          mopidy-mpris
+          mopidy-musicbox-webclient
+          # mopidy-notify
+        ];
+
+        settings = {
+          core.restore_state = true;
+          logging.verbosity = 1;
+
+          # Built-in extensions
+          file.enabled = true;
+          http = {
+            enabled = true;
+
+            hostname = "::1";
+          };
+
+          # External extensions
+          local.media_dir = musicDir;
+          mpd.hostname = "::1";
         };
       };
-    };
-  };
+
+      # services.mpdris2 = {
+      #   enable = true;
+      #
+      #   notifications = true;
+      #   musicDirectory = musicDir;
+      # };
+
+      programs.beets =
+        let
+          beetsStateDir = "${config.xdg.dataHome or "$XDG_DATA_HOME"}/beets";
+        in
+        {
+          enable = true;
+
+          settings = {
+            match = {
+              required = "title artist";
+
+              ignored_media = [
+                "Data CD"
+                "DVD"
+                "DVD-Video"
+                "Blu-ray"
+                "HD-DVD"
+                "VCD"
+                "SVCD"
+                "UMD"
+                "VHS"
+              ];
+            };
+
+            directory = musicDir;
+            library = "${beetsStateDir}/musiclibrary.db";
+
+            import = {
+              languages = "en jp";
+              log = "${beetsStateDir}/import.log";
+              move = true;
+            };
+
+            format_item = "$albumartist - $album - $track $title";
+
+            paths = {
+              default = "$albumartist - $album%aunique{}/$track - $title";
+              singleton = "etc/$artist - $title";
+              comp = "Various Artists - $album%aunique{}/$track - $title ($artist)";
+            };
+          };
+        };
+    }
+  );
 }
--- tests/homeModules/sys/music.nix
+++ tests/homeModules/sys/music.nix
@@ -6,8 +6,8 @@ with lib;
   config = {
     sys.music.enable = true;
 
-    test.stubs.mpd = { };
-    test.stubs.mpdris2 = { };
+    # test.stubs.mpd = { };
+    # test.stubs.mpdris2 = { };
 
     nmt.script = ''
       configDir="home-files/.config"
@@ -22,7 +22,7 @@ with lib;
       assertFileContains "$vimpcConfig" 'map J :tabprevious<C-M>'
       assertFileContains "$vimpcConfig" 'map K :tabnext<C-M>'
 
-      mpdConfig=$(grep -o '/nix/store/.*-mpd.conf' $TESTED/home-files/.config/systemd/user/mpd.service)
+      # mpdConfig=$(grep -o '/nix/store/.*-mpd.conf' $TESTED/home-files/.config/systemd/user/mpd.service)
       # assertFileContains "$mpdConfig" 'audio_output {\n  type "pipewire"\n  name "PipeWire Sound Server"\n}'
     '';
   };
