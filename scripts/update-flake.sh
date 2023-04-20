5a335950971d623a57de241c54a61e787a71490e
Cynthia Fox <houstdav000@gmail.com>
2023-04-20 08:12:55 -0400

    scripts/update-flake.sh: Add summary to end

--- scripts/update-flake.sh
+++ scripts/update-flake.sh
@@ -13,19 +13,43 @@ get_inputs() {
 
 INPUTS=$(get_inputs | tr -d '"')
 
+PASS=""
+FAIL=""
+
 # NOTE: No point in making this parallel as Nix will just complain
 for INPUT in $INPUTS; do
   ORIGINAL_FLAKE=$(<$LOCKFILE)
 
   if ! nix flake lock --update-input "$INPUT"; then
     echo "Unable to update input \"$INPUT\""
-
     echo "$ORIGINAL_FLAKE" >$LOCKFILE
+    FAIL="$FAIL $INPUT"
+
+    continue
   fi
 
   if ! nix flake check; then
     echo "Check for updated input \"$INPUT\" failed."
-
     echo "$ORIGINAL_FLAKE" >$LOCKFILE
+    FAIL="$FAIL $INPUT"
+
+    continue
   fi
+
+  PASS="$PASS $INPUT"
 done
+
+cat << EOF
+
+=================
+Update flake script completed.
+
+$(echo "$PASS" | wc -w) inputs successfully updated:
+$(echo "$PASS" | tr ' ' "\n")
+
+$(echo "$FAIL" | wc -w) inputs failed to update:
+$(echo "$FAIL" | tr ' ' "\n")
+
+=================
+EOF
+
