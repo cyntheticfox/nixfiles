4f710a02da0f2e4df187fad5a98a9ff2beebedda
Cynthia Fox <cyntheticfox@gh0st.sh>
2023-05-07 22:48:09 -0400

    hc/cyn-fw: Try to improve power consumption

--- nixosConfigurations/cyn-framework/hardware-configuration.nix
+++ nixosConfigurations/cyn-framework/hardware-configuration.nix
@@ -106,16 +106,16 @@
     enable = true;
 
     settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_AC = "powersave";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "balance_power";
       CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
 
       INTEL_GPU_MIN_FREQ_ON_AC = 100;
       INTEL_GPU_MIN_FREQ_ON_BAT = 100;
-      INTEL_GPU_MAX_FREQ_ON_AC = 1450;
+      INTEL_GPU_MAX_FREQ_ON_AC = 1000;
       INTEL_GPU_MAX_FREQ_ON_BAT = 1000;
-      INTEL_GPU_BOOST_FREQ_ON_AC = 1450;
+      INTEL_GPU_BOOST_FREQ_ON_AC = 1000;
       INTEL_GPU_BOOST_FREQ_ON_BAT = 1000;
 
       START_CHARGE_THRESH_BAT0 = 75;
