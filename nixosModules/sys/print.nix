21e3affdbe6f2ba2750100e7859455241a122b75
Cynthia Fox <cyntheticfox@gh0st.sh>
2023-12-03 19:30:09 -0500

    nm/sys/print: Start on modularizing

--- nixosModules/sys/print.nix
+++ nixosModules/sys/print.nix
@@ -1,36 +1,142 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, ... }:
 let
   cfg = config.sys.print;
-in
-{
-  options.sys.print.enable = lib.mkEnableOption "paper document printing and scanning";
 
-  config = lib.mkIf cfg.enable {
-    services.avahi.enable = true;
+  ippConfig = lib.types.submodule (hostName: {
+    uri = lib.mkOption {
+      types = lib.types.str;
+      default = "ipp://${hostName}.${config.networking.domain}:631/ipp";
 
-    services.printing = {
-      enable = true;
+      description = ''
+        URI to print to with IPP Everywhere.
+      '';
+    };
+  });
 
-      drivers = with pkgs; [ canon-cups-ufr2 ];
+  paperPrinter = lib.types.submodule (_: {
+    hostName = lib.mkOption {
+      type = lib.types.str;
+
+      description = ''
+        Paper printing device hostname.
+      '';
     };
 
-    hardware = {
-      printers = {
-        ensureDefaultPrinter = "cyn-print";
+    description = lib.mkOption {
+      type = lib.types.lines;
 
-        ensurePrinters = [{
-          description = "Canon ImageCLASS MF642CDW";
-          deviceUri = "ipp://192.168.2.7:631/ipp";
-          model = "everywhere";
-          name = "cyn-print";
-        }];
+      description = ''
+        Description of paper printing device.
+      '';
+    };
+
+    printDrivers = lib.mkOption {
+      type = with lib.types; listOf package;
+
+      description = ''
+        Packages required to install print device.
+      '';
+    };
+
+    config = lib.mkOption {
+      type = lib.types.oneOf [ ippConfig ];
+
+      description = ''
+        Configuration for the printer.
+      '';
+    };
+  });
+
+  # paperScanner = lib.types.submodule (_: {
+  #   hostName = lib.mkOption {
+  #     type = lib.types.str;
+  #
+  #     description = ''
+  #       Paper scanning device hostname.
+  #     '';
+  #   };
+  # });
+
+  # myPrinters = [
+  #   (
+  #     let
+  #       hostName = "cyn-print";
+  #     in
+  #     {
+  #       inherit hostName;
+  #
+  #       description = "Canon ImageCLASS MF642CDW";
+  #
+  #       config = ippConfig hostName;
+  #
+  #       printDrivers = with pkgs; [ canon-cups-ufr2 ];
+  #     }
+  #   )
+  # ];
+  #
+  # myScanDrivers = with pkgs; [ sane-airscan ];
+in
+{
+  options.sys.iot = {
+    paperPrint = {
+      enable = lib.mkEnableOption "paper document printing";
+
+      printers = lib.mkOption {
+        type = with lib.types; listOf paperPrinter;
+        default = [ ];
+
+        description = ''
+          Ordered list of printers to configure for the system.
+        '';
       };
+    };
 
-      sane = {
-        enable = true;
+    paperScan = {
+      enable = lib.mkEnableOption "paper document scanning";
 
-        extraBackends = with pkgs; [ sane-airscan ];
+      scanDrivers = lib.mkOption {
+        type = with lib.types; listOf package;
+        default = [ ];
+
+        description = ''
+          Additional packages to install to support scanners.
+        '';
       };
     };
   };
+
+  config = lib.mkMerge [
+    (lib.mkIf cfg.paperPrint.enable {
+
+      services.printing = {
+        inherit (cfg.paperPrint) enable;
+
+        drivers = builtins.zipWithAttrs (builtins.getAttr "printDrivers") cfg.paperPrint.printers;
+      };
+
+      hardware.printers =
+        let
+          printHost = "cyn-print";
+        in
+        {
+          ensureDefaultPrinter = builtins.elemAt 0 cfg.paperPrint.printers;
+
+          ensurePrinters = [{
+            description = "Canon ImageCLASS MF642CDW";
+            deviceUri = "ipp://${printHost}.${config.networking.domain}:631/ipp";
+            model = "everywhere";
+            name = "cyn-print";
+          }];
+        };
+    })
+    (lib.mkIf cfg.paperScan.enable {
+      services.avahi = { inherit (cfg.paperScan) enable; };
+
+      hardware.sane = {
+        inherit (cfg.paperScan) enable;
+
+        extraBackends = cfg.paperScan.scanDrivers;
+      };
+    })
+  ];
 }
